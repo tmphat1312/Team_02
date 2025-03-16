0dff88e8629a3621cf02f327ba649f6e0a232381
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-16 05:04:16 +0000

    refactor: port iam service to cloudflare worker

--- 02_Source/01_Source_Code/services/iam/src/index.ts
+++ 02_Source/01_Source_Code/services/iam/src/index.ts
@@ -1,18 +1,9 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get("/", (c) => {
-  return c.text("Hello Hono from iam service!");
-});
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
+export default app
