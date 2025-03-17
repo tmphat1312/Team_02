12091c2730a6f607604459a355af6aeff732ddeb
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-17 22:57:08 +0000

    refactor(iam): make iam api-gateway

--- 02_Source/01_Source_Code/services/iam/env.ts
+++
@@ -1,8 +0,0 @@
-import { Env } from "@adonisjs/env";
-
-export const env = await Env.create(new URL("./", import.meta.url), {
-  PORT: Env.schema.number(),
-  DATABASE_URL: Env.schema.string(),
-  BETTER_AUTH_SECRET: Env.schema.string(),
-  NODE_ENV: Env.schema.string.optional(),
-});
