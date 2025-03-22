99b635d2df330e5d5516c95eee3e3197a82def11
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-22 10:40:12 +0000

    chore(admin): setup prime react

--- 02_Source/01_Source_Code/apps/admin/src/main.tsx
+++ 02_Source/01_Source_Code/apps/admin/src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
 
 import './index.css';
+import 'primereact/resources/themes/tailwind-light/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 import { RoutesProvider } from './RoutesProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RoutesProvider />
+    <PrimeReactProvider>
+      <RoutesProvider />
+    </PrimeReactProvider>
   </StrictMode>
 );
