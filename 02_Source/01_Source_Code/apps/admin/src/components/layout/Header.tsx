aeef500716fac22e7dba647cec0e1f161b031b50
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-23 11:01:30 +0000

    feat(admin): create a text logo for minimal log presentation

--- 02_Source/01_Source_Code/apps/admin/src/components/layout/Header.tsx
+++ 02_Source/01_Source_Code/apps/admin/src/components/layout/Header.tsx
@@ -1,10 +1,10 @@
-import { Logo } from '../Logo';
+import { TextLogo } from '../TextLogo';
 import { UserAvatar } from '../UserAvatar';
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between p-4">
-      <Logo />
+    <header className="flex items-center justify-between px-6 py-2">
+      <TextLogo />
       <UserAvatar />
     </header>
   );
