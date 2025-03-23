a7ea0270825524f31e1fc5930b63f89f679a2ca2
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-23 12:18:09 +0000

    feat(admin): include email to user avatar comp

--- 02_Source/01_Source_Code/apps/admin/src/components/UserAvatar.tsx
+++ 02_Source/01_Source_Code/apps/admin/src/components/UserAvatar.tsx
@@ -1,8 +1,16 @@
+import { authClient } from '../lib/auth-client';
 import { LogoutButton } from './LogoutButton';
 
 export function UserAvatar() {
+  const { isPending, data } = authClient.useSession();
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
+    <div className="flex items-center gap-2">
+      <div>{data?.user.email}</div>
       <LogoutButton />
     </div>
   );
