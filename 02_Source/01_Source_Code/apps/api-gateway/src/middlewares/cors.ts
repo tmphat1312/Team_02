958e5f9acde96fb0df4b45eda12ea9c4313b424d
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-25 23:00:57 +0000

    fix(api-gateway): allow all origins to access api gateway

--- 02_Source/01_Source_Code/apps/api-gateway/src/middlewares/cors.ts
+++ 02_Source/01_Source_Code/apps/api-gateway/src/middlewares/cors.ts
@@ -3,13 +3,14 @@ import { env } from "hono/adapter";
 import { cors } from "hono/cors";
 
 export const corsMiddleware: Handler = async (c, next) => {
-  const { ADMIN_APP_URL, WEB_APP_URL } = env<{
-    ADMIN_APP_URL: string;
-    WEB_APP_URL: string;
-  }>(c);
+  // const { ADMIN_APP_URL, WEB_APP_URL } = env<{
+  //   ADMIN_APP_URL: string;
+  //   WEB_APP_URL: string;
+  // }>(c);
   const corsMiddlewareHandler = cors({
-    origin: [ADMIN_APP_URL, WEB_APP_URL],
-    allowMethods: ["POST", "GET", "OPTIONS"],
+    origin: ["*"],
+    allowMethods: ["POST", "GET", "OPTIONS", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
     maxAge: 600,
     credentials: true,
   });
