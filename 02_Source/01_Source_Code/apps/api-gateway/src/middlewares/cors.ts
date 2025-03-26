c7f2dc02caa688e25d5a9653dac1e511613ab773
Truong Minh Phat <tmphat1312@gmail.com>
2025-03-26 14:33:47 +0000

    fix(cors): use minimal cors settings

--- 02_Source/01_Source_Code/apps/api-gateway/src/middlewares/cors.ts
+++ 02_Source/01_Source_Code/apps/api-gateway/src/middlewares/cors.ts
@@ -1,4 +1,3 @@
-import consola from "consola";
 import type { Handler } from "hono";
 import { env } from "hono/adapter";
 import { cors } from "hono/cors";
@@ -10,13 +9,9 @@ export const corsMiddleware: Handler = async (c, next) => {
   }>(c);
   const corsMiddlewareHandler = cors({
     origin: [ADMIN_APP_URL, WEB_APP_URL],
-    allowMethods: ["POST", "GET", "OPTIONS", "PUT", "PATCH", "DELETE"],
     allowHeaders: ["Content-Type", "Authorization"],
-    maxAge: 600,
     credentials: true,
   });
-
-  consola.info(ADMIN_APP_URL, WEB_APP_URL);
 
   return corsMiddlewareHandler(c, next);
 };
