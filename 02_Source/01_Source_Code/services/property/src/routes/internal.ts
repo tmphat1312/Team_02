b2d15cf9f1418897648063c2efd88814468862c0
Truong Minh Phat <tmphat1312@gmail.com>
2025-04-01 04:14:31 +0000

    refactor(property): simplify internal route to check for service health

--- 02_Source/01_Source_Code/services/property/src/routes/internal.ts
+++ 02_Source/01_Source_Code/services/property/src/routes/internal.ts
@@ -2,18 +2,10 @@ import { Hono } from "hono";
 
 const route = new Hono();
 
-route.get("/", (c) => {
-  return c.text("Hello from Property Service!");
-});
+route.get("/", (c) => c.text("Hello from Property Service!"));
 
-route.get("/health", (c) => {
-  const timestamp = new Date().toISOString();
-  return c.json({ status: "OK", timestamp });
-});
-
-route.get("/status", (c) => {
-  const timestamp = new Date().toISOString();
-  return c.json({ message: "Service is running", timestamp });
-});
+route.get("/status", (c) =>
+  c.text(`Service is running on ${new Date().toISOString()}`)
+);
 
 export const internalRoute = route;
