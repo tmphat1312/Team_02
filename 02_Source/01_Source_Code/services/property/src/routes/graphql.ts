bf941b9e18b7fed7c4f696af4e22054dab05ef6c
Truong Minh Phat <tmphat1312@gmail.com>
2025-04-02 13:51:45 +0000

    feat(property): create graphql resources of rules

--- 02_Source/01_Source_Code/services/property/src/routes/graphql.ts
+++ 02_Source/01_Source_Code/services/property/src/routes/graphql.ts
@@ -1,14 +1,17 @@
-import { amenitiesTable, categoriesTable } from "../db/schema";
+import { amenitiesTable, categoriesTable, rulesTable } from "../db/schema";
 import { resourceFactory } from "../utils/resource-factory";
 
 const amenityResources =
   resourceFactory<typeof amenitiesTable.$inferSelect>(amenitiesTable);
 const categoryResources =
   resourceFactory<typeof categoriesTable.$inferSelect>(categoriesTable);
+const ruleResources =
+  resourceFactory<typeof rulesTable.$inferSelect>(rulesTable);
 
 export const Query = {
   amenities: amenityResources.index,
   categories: categoryResources.index,
+  rules: ruleResources.index,
 };
 
 export const Mutation = {};
--- 02_Source/01_Source_Code/services/property/src/routes/properties.ts
+++
@@ -1,9 +0,0 @@
-import { routeFactory } from "../utils/route-factory";
-
-const route = routeFactory.createApp();
-
-route.post("/", (c) => {
-  return c.text("Create Property");
-});
-
-export const propertiesRoute = route;
