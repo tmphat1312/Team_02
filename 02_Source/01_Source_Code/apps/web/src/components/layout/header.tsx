e13149495b470bb4da33aa429eb2b1895f3a2fc7
Truong Minh Phat <tmphat1312@gmail.com>
2025-04-07 03:13:32 +0000

    chore(web): setup routes and core structure to work with next.js

--- 02_Source/01_Source_Code/apps/web/src/components/layout/header.tsx
+++ 02_Source/01_Source_Code/apps/web/src/components/layout/header.tsx
@@ -1,7 +1,11 @@
 export function Header() {
   return (
-    <div>
-      <h1>Header Component</h1>
+    <div className="container mx-auto">
+      <header className="flex justify-between items-center py-4">
+        <div>Logo</div>
+        <div>Search</div>
+        <div>Profile</div>
+      </header>
     </div>
   );
 }
