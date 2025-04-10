bc6a93dd6683ccc63088cc3ec66320ca88aa37e6
TrinhMinhThuan <trinhminhthuan059@gmail.com>
2025-04-11 01:52:46 +0700

    feat(verification-email): send OTP and verify OTP

---
+++ 02_Source/01_Source_Code/services/verification/src/lib/auth.ts
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { admin } from "better-auth/plugins";
+import pg from "pg";
+
+export const ROLES = ["user", "admin", "host"] as const;
+
+export const auth = betterAuth({
+  advanced: {
+    defaultCookieAttributes: {
+      secure: true,
+      httpOnly: true,
+      sameSite: "none", // Allows CORS-based cookie sharing across subdomains
+      partitioned: true, // New browser standards will mandate this for foreign cookies
+    },
+  },
+  user: {
+    additionalFields: {
+      isHost: {
+        type: "boolean",
+        required: false,
+        defaultValue: false,
+      },
+    },
+  },
+  database: new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [admin()],
+  trustedOrigins: [process.env.WEB_APP_URL!, process.env.ADMIN_APP_URL!],
+  secret: process.env.BETTER_AUTH_SECRET!,
+});
