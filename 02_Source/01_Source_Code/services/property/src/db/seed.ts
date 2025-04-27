529f052709ef27e431b969a9b98d58624896c2e9
21120581 <thuytruc011258@gmail.com>
2025-04-27 22:14:16 +0700

    chore(property): modify data and seed function

--- 02_Source/01_Source_Code/services/property/src/db/seed.ts
+++ 02_Source/01_Source_Code/services/property/src/db/seed.ts
@@ -1,4 +1,7 @@
 import { consola } from "consola";
+import { sql } from "drizzle-orm";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
 
 import { db } from "./";
 import { amenitiesTable, categoriesTable, rulesTable } from "./schema";
@@ -7,26 +10,44 @@ import categories from "./data/categories.json";
 import amenities from "./data/amenities.json";
 import commonRules from "./data/common-rules.json";
 
-async function seedDatabase() {
-  await Promise.try(async () => {
-    await db.delete(amenitiesTable);
-    await db.delete(categoriesTable);
-    await db.delete(rulesTable);
-  }).catch((error) => {
-    consola.error("Error deleting tables", error);
-  });
+const execPromise = promisify(exec);
 
-  await Promise.try(async () => {
-    await Promise.all([
-      db.insert(categoriesTable).values(categories),
-      db.insert(amenitiesTable).values(amenities),
-      db.insert(rulesTable).values(commonRules),
-    ]);
+async function dropTables() {
+  await Promise.all([
+    // db.execute(sql`DROP TABLE IF EXISTS property_categories CASCADE`),
+    // db.execute(sql`DROP TABLE IF EXISTS property_amenities CASCADE`),
+    // db.execute(sql`DROP TABLE IF EXISTS property_rules CASCADE`),
+
+    db.execute(sql`DROP TABLE IF EXISTS categories CASCADE`),
+    db.execute(sql`DROP TABLE IF EXISTS amenities CASCADE`),
+    db.execute(sql`DROP TABLE IF EXISTS rules CASCADE`),
+  ]);
+}
+
+async function insertSeedData() {
+  await Promise.all([
+    db.insert(categoriesTable).values(categories),
+    db.insert(amenitiesTable).values(amenities),
+
+    db.insert(rulesTable).values(
+      commonRules.map((rule) => ({
+        ...rule,
+        type: "common" as const,
+      }))
+    ),
+  ]);
+}
+
+async function seedDatabase() {
+  try {
+    await dropTables();
+    await execPromise("bunx drizzle-kit push");
+    await insertSeedData();
 
     consola.success("Database seeded successfully");
-  }).catch((error) => {
-    consola.error("Error seeding database", error);
-  });
+  } catch (error) {
+    consola.error("Error seeding database: ", error);
+  }
 }
 
 seedDatabase();
